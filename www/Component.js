593b97873d03e426343033243c6fabb40c8947f8
Donovan Isherwood <donovan.isherwood@gmail.com>
2016-03-21 14:09:31 +0100

    Added the main view and adjusted the config

--- www/Component.js
+++ www/Component.js
@@ -5,7 +5,7 @@ sap.ui.define([
 ], function(UIComponent, Device, models) {
 	"use strict";
 
-	return UIComponent.extend("sdc.Component", {
+	return UIComponent.extend("Component", {
 
 		metadata: {
 			manifest: "json"
